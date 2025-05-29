9a742955f3662b6ff2ebe2f3d339a7b4551888c1
roman <haydenkfalcon@gmail.com>
2025-05-29 18:52:43 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -85,7 +85,12 @@ async def seedbean(ctx):
 async def quit(ctx):
     sys.exit()
 
-
+banned_words = ["FUCK", "STUPID", "FAT"]
+@bot.event()
+async def on_message(message):
+    for word in banned_words:
+        if word in message.content.lower() or word in message.content.upper():
+            await message.channel.send(f"[message.author.mention] Don't say bad words !!!!!!")
     
 
 @bot.event
