9b878239bd9b3e77450392d7671573a2974b00f9
roman <haydenkfalcon@gmail.com>
2025-05-29 18:57:24 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -86,7 +86,7 @@ async def quit(ctx):
     sys.exit()
 
 banned_words = ["FUCK", "STUPID", "FAT"]
-@client.event()
+@bot.event()
 async def on_message(message):
     for word in banned_words:
         if word in message.content.lower() or word in message.content.upper():
