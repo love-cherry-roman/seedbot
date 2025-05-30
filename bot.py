3594856eccbf1cd4d78ed0369cc83b4862f0136b
roman <haydenkfalcon@gmail.com>
2025-05-29 20:43:55 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,9 @@ from discord import app_commands
 import random
 import math 
 import os
-
+from discord.ext import commands
+from datetime import datetime, time, timedelta
+import asyncio
 
 
 
@@ -29,8 +31,31 @@ keep_alive()
 
 banned_words = ["FUCK", "STUPID", "FAT", "fuck", "stupid", "fat"]
 
+WHEN = time(20, 45, 0)  # 6:00 PM
+channel_id = 1339429895889223773
 
-    
+async def called_once_a_day():
+    await bot.wait_until_ready()  # Make sure your guild cache is ready so the channel can be found via get_channel
+    channel = bot.get_channel(channel_id) # Note: It's more efficient to do bot.get_guild(guild_id).get_channel(channel_id) as there's less looping involved, but just get_channel still works fine
+    await channel.send("hi(it worked)")
+
+async def background_task():
+    now = datetime.utcnow()
+    if now.time() > WHEN:  # Make sure loop doesn't start after {WHEN} as then it will send immediately the first time as negative seconds will make the sleep yield instantly
+        tomorrow = datetime.combine(now.date() + timedelta(days=1), time(0))
+        seconds = (tomorrow - now).total_seconds()  # Seconds until tomorrow (midnight)
+        await asyncio.sleep(seconds)   # Sleep until tomorrow and then the loop will start 
+    while True:
+        now = datetime.utcnow() # You can do now() or a specific timezone if that matters, but I'll leave it with utcnow
+        target_time = datetime.combine(now.date(), WHEN)  # 6:00 PM today (In UTC)
+        seconds_until_target = (target_time - now).total_seconds()
+        await asyncio.sleep(seconds_until_target)  # Sleep until we hit the target time
+        await called_once_a_day()  # Call the helper function that sends the message
+        tomorrow = datetime.combine(now.date() + timedelta(days=1), time(0))
+        seconds = (tomorrow - now).total_seconds()  # Seconds until tomorrow (midnight)
+        await asyncio.sleep(seconds)   # Sleep until tomorrow and then the loop will start a new iteration
+
+
 
 @bot.command()
 async def hello(ctx):
